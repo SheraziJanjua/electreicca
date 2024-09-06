1204f6857216549098d10a352965aef4ebfe6691
SheraziJanjua <abubakarsheraz370@gmail.com>
2024-09-06 05:02:24 -0700

    Next Button color change

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import logo from '../assets/logo.jpg'; 
 import image from '../assets/shoes.jpg';
@@ -20,82 +20,79 @@ const Login = () => {
     setSubmitting(false);
   };
 
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
   return (
     <div className="flex h-screen">
-      <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start md:ml-8 p-8">
-        <div className="flex items-center absolute top-12 left-[40%] md:left-14">
-          <img src={logo} alt="Logo" className="w-10 h-10 rounded-full mr-4" /> {/* Logo image */}
-          <h1 className="text-2xl font-bold">Logo</h1> {/* Logo text */}
+      <div className="w-full md:w-1/2 flex flex-col items-center md:items-start p-8">
+        <div className="w-full flex justify-center md:justify-start mb-8">
+          <div className="block md:flex items-center">
+            <img src={logo} alt="Logo" className="w-10 h-10 rounded-full mr-4" /> 
+            <h1 className="text-xl md:text-2xl font-bold">Logo</h1>
+          </div>
         </div>
-        
-        <div className="w-full max-w-sm mt-auto mb-auto">
-          <h1 className="text-2xl font-bold mb-6">Log in</h1>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-          >
-            {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-              <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="email@example.com"
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                  />
-                  {errors.email && touched.email && (
-                    <div className="text-red-500 text-sm mt-1">{errors.email}</div>
-                  )}
-                </div>
-                <div className="mb-6">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="enter your password"
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                  />
-                  {errors.password && touched.password && (
-                    <div className="text-red-500 text-sm mt-1">{errors.password}</div>
-                  )}
-                </div>
-                <div className="flex flex-col justify-between">
-                  <button
-                    className="bg-yellow-500 w-[60%] mb-4 hover:bg-yellow-600 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Login
-                  </button> 
-                  <a
-                    className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                    href="#"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-                <div className="mt-8">
-                  <p className="text-sm">
-                    Don't have an account? <a href="#" className="text-blue-500">Register here</a>
-                  </p>
-                </div>
-              </form>
-            )}
-          </Formik>
+        <div className="block md:flex flex-col justify-center h-full max-w-sm w-full">
+          <h1 className="block text-xl md:text-2xl font-bold mb-6">Log in</h1>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email@example.com"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {formik.errors.email && formik.touched.email && (
+                <div className="text-red-500 text-sm mt-1">{formik.errors.email}</div>
+              )}
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="enter your password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {formik.errors.password && formik.touched.password && (
+                <div className="text-red-500 text-sm mt-1">{formik.errors.password}</div>
+              )}
+            </div>
+            <div className="flex flex-col justify-between">
+              <button
+                className="bg-yellow-500 w-[60%] mb-4 hover:bg-yellow-600 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+                type="submit"
+                disabled={formik.isSubmitting}
+              >
+                Login
+              </button> 
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="#"
+              >
+                Forgot password?
+              </a>
+            </div>
+            <div className="mt-8">
+              <p className="text-sm">
+                Don't have an account? <a href="#" className="text-blue-500">Register here</a>
+              </p>
+            </div>
+          </form>
         </div>
       </div>
 
@@ -104,8 +101,8 @@ const Login = () => {
         style={{
           backgroundImage: `url(${image})`,
           backgroundSize: 'cover',
-                         
-          }}
+          height: '101vh',
+        }}
       ></div>
     </div>
   );
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -9,7 +9,7 @@ const Sliderr = () => {
   const sliderRef = useRef(null);
   const NextArrow = () => (
     <div
-      className="bg-yellow-500 w-8 h-6 md:w-10 md:h-6 cursor-pointer  text-black hover:bg-white rounded-lg absolute bottom-[20%] right-4 md:bottom-[25%] lg:bottom-[22%] md:right-4 lg:right-[-10px] z-10 sm:right-2"
+      className="bg-yellow-500 w-8 h-6 md:w-10 md:h-6 cursor-pointer  text-black hover:bg-white rounded-lg absolute bottom-[25%] right-4 md:bottom-[25%] lg:bottom-[27%] md:right-4 lg:right-[-10px] z-10 sm:right-2"
       onClick={() => sliderRef.current.slickNext()}
     >
       <div style={{ display: 'flex',justifyContent: 'center', height: '100%', marginTop:'-1.5px' }}>
@@ -20,7 +20,7 @@ const Sliderr = () => {
   
   const PrevArrow = () => (
     <div
-      className="bg-yellow-500 w-8 h-6 md:w-10 md:h-6 cursor-pointer text-black hover:bg-white rounded-lg absolute bottom-[20%] left-4 md:bottom-[25%] lg:bottom-[22%] md:left-4 lg:left-[-10px] z-10 sm:left-2"
+      className="bg-yellow-500 w-8 h-6 md:w-10 md:h-6 cursor-pointer text-black hover:bg-white rounded-lg absolute bottom-[25%] left-4 md:bottom-[25%] lg:bottom-[27%] md:left-4 lg:left-[-10px] z-10 sm:left-2"
       onClick={() => sliderRef.current.slickPrev()}
     >
       <div style={{ display: 'flex', marginTop:'-1.5px', height: '100%',justifyContent: 'center' }}>
@@ -52,7 +52,7 @@ const Sliderr = () => {
                 <h2 className="text-white mt-2 md:mt-6 px-[6px] text-sm tracking-normal md:text-lg lg:text-xl lg:whitespace-nowrap">
                   {data.text}
                 </h2>
-                <div className=' mb-4 mt-6'>
+                <div className=' mb-4 mt-8'>
                 <Link 
                  to='/login'
                  className="px-6 py-1 bg-yellow-500 text-black rounded-full text-lg font-semibold  transition duration-300 ease-in-out hover:bg-orange-500 glow-effect">
